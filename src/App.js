c8e395350c83fb971d29f190a62f34ca1f8f6314
Akash Dubey <akashdubey7892@gmail.com>
2023-07-25 19:30:18 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -59,13 +59,11 @@ export default function App() {
         <div className=' shadow rounded infor'>
           <img src='https://purepng.com/public/uploads/large/purepng.com-weather-iconsymbolsiconsapple-iosiosios-8-iconsios-8-721522596142qx4ep.png' className='img1' alt="Sorry" />
           <h5 className='weatherCity'>{data?.name}</h5>
-          <h6 className='descri'>{data.weather[0].description}</h6>
-
-          <div className="container" style={{ marginTop: 40 }}>
+          <h6 className='descri'>{data.weather[0].description}</h6>          <div className="container" style={{ marginTop: 40 }}>
             <div className="row align-items-start">
               <div className="col">
                 <h6>Temperature</h6>
-                <h5 className='weatherTemp'>{((data?.main?.temp) - 273.15).toFixed(2)}C</h5>
+                <h5 className='weatherTemp'>{((data?.main?.temp) - 273.15).toFixed(2)}°C</h5>
               </div>
               <div className="col">
                 <h6>Humidity</h6>
